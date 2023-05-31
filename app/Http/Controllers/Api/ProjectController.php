07ccd7cb21c8dd99059c5e65635797ac287f0619
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-31 14:11:54 +0200

    fix: result property in object json

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -31,7 +31,7 @@ class ProjectController extends Controller
         if($project){
             return response()->json([
                 'success' => true,
-                'project' => $project,
+                'result' => $project,
             ]);
         } else{
             // altrimenti mando un messaggio di errore che gestirò dal front end
