6fc37195ce5c94bd7c335ed896edec601b7bd71f
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-31 14:13:25 +0200

    fix: name property json

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -19,7 +19,7 @@ class ProjectController extends Controller
 
         return response()->json([
             'success' => true,
-            'results' => $projects
+            'project' => $projects
         ]);
     }
 
