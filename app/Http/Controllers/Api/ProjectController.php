9ee7a3922e0a278baa469a53dd241b741a2868a8
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-31 14:45:48 +0200

    fix: renamed index json property

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -19,7 +19,7 @@ class ProjectController extends Controller
 
         return response()->json([
             'success' => true,
-            'project' => $projects
+            'results' => $projects
         ]);
     }
 
